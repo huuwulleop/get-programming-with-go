e5e6d7ecb3a1bd60b1c33356dc96214c517e390d
VP <huuwulleop@hotmail.com>
2022-04-23 14:07:47 -0400

    Add random testings

--- src/05tickettomars/main.go
+++ src/05tickettomars/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var companies = []string{"AAA", "BBB", "CCC"}
 
 func main() {
-	fmt.Println("Test")
+	fmt.Printf("=== Test ===\n\n")
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 10; i++ {
+		num := rand.Intn(80)
+		fmt.Println(num)
+	}
+
+	fmt.Println()
+
+	fmt.Println(len(companies))
+
+	for i := 0; i < 10; i++ {
+		ind := rand.Intn(3)
+		fmt.Println(companies[ind])
+	}
 }
